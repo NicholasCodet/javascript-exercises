24aa4d24316ee4cae626b45b382d28cf70cb3646
Nicholas <124813384+NicholasCodet@users.noreply.github.com>
2025-03-25 15:16:01 +0100

    complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (content, times) {
+  let string = "";
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < times; i++) {
+      string += content;
+    }
+    return string;
+  }
 };
 
 // Do not edit below this line

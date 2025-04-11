93cb1784c47da1f4a707d576bed4e35cf6725655
Nicholas <124813384+NicholasCodet@users.noreply.github.com>
2025-04-11 16:57:42 +0200

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => characters.includes(character))
+    .join("");
+  const reversedString = cleanedString.split("").reverse().join("");
 
+  if (cleanedString === reversedString) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
